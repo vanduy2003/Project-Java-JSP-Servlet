3274236659394302c11e4325ffcc3c97af296ee3
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-06-08 19:03:43 +0700

    Init commit

---
+++ src/main/java/com/webfruit/model/CommonDao.java
@@ -0,0 +1,11 @@
+package com.webfruit.model;
+
+import java.util.ArrayList;
+
+public interface CommonDao <T> {
+    public boolean insert (T T);
+    public boolean update (T T);
+    public boolean delete (String id);
+    public T selectById (String id);
+    public ArrayList<T> selectAll();
+}
